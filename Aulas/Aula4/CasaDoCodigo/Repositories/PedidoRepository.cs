659928ec2a0916ec9de1af7925788c0b0424e6e0
MatheusPetrachin <mrp.petrachin@gmail.com>
2020-01-30 21:56:08 -0300

    Gravando_Id_Do_Pedido_Na_Sessao

--- Aulas/Aula4/CasaDoCodigo/Repositories/PedidoRepository.cs
+++ Aulas/Aula4/CasaDoCodigo/Repositories/PedidoRepository.cs
@@ -9,7 +9,8 @@ namespace CasaDoCodigo.Repositories
 {
     public interface IPedidoRepository
     {
-
+        Pedido GetPedido();
+        void AddItem(string codigo);
     }
 
     public class PedidoRepository : BaseRepository<Pedido>, IPedidoRepository
@@ -19,6 +20,52 @@ namespace CasaDoCodigo.Repositories
         public PedidoRepository(ApplicationContext contexto, IHttpContextAccessor contextAcessor) : base(contexto)
         {
             this.contextAcessor = contextAcessor;
+        }
+
+        public void AddItem(string codigo)
+        {
+            var produto = contexto.Set<Produto>()
+                .Where(p => p.Codigo == codigo)
+                .SingleOrDefault();
+
+            if(produto == null)
+            {
+                throw new ArgumentException("Produto Não Encontrado");
+            }
+
+            var pedido = GetPedido();
+
+            var itemPedido = contexto.Set<ItemPedido>()
+                .Where(i => i.Produto.Codigo == codigo
+                && i.Pedido.Id == pedido.Id)
+                .SingleOrDefault();
+
+            if (itemPedido == null)
+            {
+                itemPedido = new ItemPedido(pedido, produto, 1, produto.Preco);
+                contexto.Set<ItemPedido>()
+                    .Add(itemPedido);
+
+                contexto.SaveChanges(); 
+            }
+        }
+
+        public Pedido GetPedido()
+        {
+            var pedidoId = GetPedidoId();
+            var pedido = dbSet
+                .Where(p => p.Id == pedidoId)
+                .SingleOrDefault();
+
+            if (pedido == null)
+            {
+                pedido = new Pedido();
+
+                dbSet.Add(pedido);
+                contexto.SaveChanges();
+                SetPedidoId(pedido.Id);
+            }
+            return pedido;
         }
 
         private int? GetPedidoId()
