6c36b4297e02c734b130fa3a3073cc22f66e7d59
MatheusPetrachin <mrp.petrachin@gmail.com>
2020-02-04 15:53:48 -0300

    Atualizar altomaticamente Quantidade e subtotal

--- Aulas/Aula4/CasaDoCodigo/wwwroot/js/carrinho.js
+++ Aulas/Aula4/CasaDoCodigo/wwwroot/js/carrinho.js
@@ -34,9 +34,16 @@
             contentType: 'application/json',
             data: JSON.stringify(data)
         }).done(function (response) {
-            location.reload();
-            });
+            let itemPedido = response.itemPedido;
+            let linhaDoItem = $('[item-id=' + itemPedido.id + ']');
+            linhaDoItem.find('input').val(itemPedido.quantidade);
+            linhaDoItem.find('[subtotal]').html((itemPedido.subtotal).duasCasas());
+        });
     }
 }
 
 carrinho = new Carrinho();
+
+Number.prototype.duasCasas = function () {
+    return this.toFixed(2).replace('.', ',');
+}
